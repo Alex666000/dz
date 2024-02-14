396f2a478f4809b097f75e0f05e72d96094d8be1
Alex666000 <bogdanov777000@mail.ru>
2024-02-14 21:56:13 +0300

    hw4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,18 +1,16 @@
 import {ChangeEvent} from "react";
-import {Simulate} from "react-dom/test-utils";
-import input = Simulate.input;
 
 type InputPropsType = {
 	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: (value: string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText:(title:string)=>void// НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
 	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-		props.setCurrentText(event.target.value)
+		props.setCurrentText(event.currentTarget.value)
 	};
 
-	return (
-	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	);
+	 return (
+	   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	 );
 };
